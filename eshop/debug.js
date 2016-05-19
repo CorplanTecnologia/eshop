6afa7962934da130affb8e7fd02d8b6830c9b3b1
Herve Prot <herve.prot@leandco.fr>
2016-05-19 16:10:38 +0200

    First part integrate new Theme keen

--- eshop/debug.js
+++ eshop/debug.js
@@ -3,6 +3,7 @@ var options = {};
 
 // options.ip = '127.0.0.1';
 // options.port = parseInt(process.argv[2]);
+options.port = 8001;
 // options.config = { name: 'total.js' };
 // options.https = { key: fs.readFileSync('keys/agent2-key.pem'), cert: fs.readFileSync('keys/agent2-cert.pem')};
 // options.sleep = 3000;
