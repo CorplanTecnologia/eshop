6b49dfa23f4f5e6a0501134d3e83874cb6bec6f2
Herve Prot <herve.prot@leandco.fr>
2016-05-23 14:12:48 +0200

    add shopping cart

--- eshop-mongodb/themes/keen/index.js
+++ eshop-mongodb/themes/keen/index.js
@@ -1,5 +1,5 @@
 exports.install = function () {
-    F.merge('/keen/css/default.css', '=keen/public/css/components.css','=keen/public/css/slider.css','=keen/public/css/style-shop.css','=keen/public/css/style.css','=keen/public/css/style-responsive.css','=keen/public/css/themes/red.css','=keen/public/css/custom.css' , '=keen/public/css/default.css');
+    F.merge('/keen/css/default.css', '/css/ui.css', '=keen/public/css/components.css', '=keen/public/css/slider.css', '=keen/public/css/style-shop.css', '=keen/public/css/style.css', '=keen/public/css/style-responsive.css', '=keen/public/css/themes/red.css', '=keen/public/css/custom.css', '=keen/public/css/default.css');
     F.merge('/keen/js/default.js', '=keen/public/js/back-to-top.js', '=keen/public/js/layout.js', '/js/jctajr.min.js', '/js/ui.js', '=keen/public/js/default.js');
 };
 
